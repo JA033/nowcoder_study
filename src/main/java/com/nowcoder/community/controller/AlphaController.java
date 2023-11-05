f0952e3a1f3632b450117a622c36fb9fb835130b
JA033 <yunxiaomeng666@gmail.com>
2023-11-05 20:34:06 +0800

    牛客论坛第二章 生成验证码

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -1,8 +1,13 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.util.CommunityUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping("/alpha")
@@ -11,5 +16,15 @@ public class AlphaController {
     @ResponseBody
     public String sayHello(){
         return "Hello World";
+    }
+
+    @RequestMapping(path="/kaptchatest",method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response){
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        cookie.setPath("/community/alpha");
+        cookie.setMaxAge(600);
+        response.addCookie(cookie);
+        return "set cookie";
     }
 }
