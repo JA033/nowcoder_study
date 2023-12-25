08d65ad2a5f578b560d39dba2a07bcc380b94a81
JA033 <yunxiaomeng666@gmail.com>
2023-12-25 20:53:18 +0800

    牛客论坛第五章 显示系统通知

--- src/main/java/com/nowcoder/community/dao/MessageMapper.java
+++ src/main/java/com/nowcoder/community/dao/MessageMapper.java
@@ -28,4 +28,16 @@ public interface MessageMapper {
 
     //修改一系列消息的状态
     int updateStatus(List<Integer> ids,int status);
+
+    //查询某个主题下最新的一条通知
+    Message selectLatestNotice(int userId,String topic);
+
+    //查询某个主题包含的通知数量
+    int selectNoticeCount(int userId,String topic);
+
+    //查询未读的通知的数量
+    int selectNoticeUnreadCount(int userId,String topic);
+
+    //查询某个主题包含的通知列表
+    List<Message> selectNotices(int userId,String topic,int offset,int limit);
 }
